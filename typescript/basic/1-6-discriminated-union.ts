64012ed19c9b12bf5a23ebc3659411e6eed23fb5
lcg-howoo <geun@howoocast.com>
2022-06-27 11:52:42 +0900

    discriminated-union 정리

---
+++ typescript/basic/1-6-discriminated-union.ts
@@ -0,0 +1,44 @@
+{
+    //discriminated union 유니온 타입에 차별화되는 이름이 동일한  타입을 둠으로써 간편하게 구분할 수 있다.
+
+    // funciton: login -> success or fail;
+    type LoginState = SucessState | FailState
+
+    type Result = 'success' | "fail";
+    type SucessState = {
+        result: 'success';
+        response: {
+            body: string;
+        }
+    }
+
+    type FailState = {
+        result: 'fail';
+        reason: string;
+    }
+
+
+    function login(id: string, password: string): Promise<LoginState>{
+        return new Promise((resolve, reject) => {
+            resolve({
+                result: "success",
+                response: {
+                    body: "login in!",
+                }
+            });
+        })
+    }
+
+    // printLoginState(state: LoginState)
+    // success -> congratulation
+    // fail -> here to be sad
+    function printLoginState(state: LoginState): void{
+        if(state.result === 'success'){ //response가 state안에 있는 경우
+            console.log(state.response.body);
+        }else{
+            console.log(state.reason);
+        }
+    }
+
+
+}
