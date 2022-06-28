d12290ae9a8f4e2eb6f0d4adfde14b7af4b1e2b1
lcg-howoo <geun@howoocast.com>
2022-06-28 17:22:53 +0900

    conditional type

---
+++ typescript/advanced/9-4-condition.ts
@@ -0,0 +1,15 @@
+{
+    type Check<T> = T extends string ? boolean : number;
+    type Type = Check<string>; // boolean string을 상속 하면 boolean 아니면 number
+
+    type TypeName<T> =
+        T extends string ? 'string'
+            : T extends number ? 'number'
+                : T extends boolean ? 'boolean'
+                    : T extends undefined ? 'undefined'
+                        : T extends Function ? 'function' : 'object'
+
+    type T0 = TypeName<string>; // string
+    type T1 = TypeName<'a'>; // string(문자열도)
+    type T2 = TypeName<() => void>; // function
+}
